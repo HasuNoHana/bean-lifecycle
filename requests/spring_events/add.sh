600e47d983cffd490d25544e73c1ffbd8480e39b
HasuNoHana <zuzannasantorowska@gmail.com>
2025-11-24 15:46:37 +0100

    add calculator

---
+++ requests/spring_events/add.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:8080/v1/calculator/add \
+  -H "Content-Type: application/json" \
+  -d '{"value": "2"}'
---
+++ requests/spring_events/multiply.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:8080/v1/calculator/multiply \
+  -H "Content-Type: application/json" \
+  -d '{"value": "2"}'
